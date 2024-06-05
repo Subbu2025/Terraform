160a5d082363d9ed3e33dededf4a62d3d25ce063
Subbu2025 <165556941+Subbu2025@users.noreply.github.com>
2024-06-05 12:35:13 +0530

    Update networking.tf

--- Modules-Demo/Modules/vpc/networking.tf
+++ Modules-Demo/Modules/vpc/networking.tf
@@ -1,1 +1,12 @@
+# To create a VPC
+resource "aws_vpc" "Proj1-vpc" {
+  cidr_block = "${var.vpc_cidr_block}"
+  tags = {Name="proj1-vpc"}
+}
 
+# To create a Public Subnet
+resource "aws_subnet" "proj1-pub-subnet" {
+  vpc_id = "${var.vpc_id}"
+  cidr_block = "${var.subnet_cidr}"
+   tags = {Name="proj1-vpc-pub-subnet"}
+}
